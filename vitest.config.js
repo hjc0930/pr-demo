bac07febb219a8668cf3e2e8e1fd1270db6afa93
hjc <hjc_0827@outlook.com>
2026-03-01 14:01:57 +0800

    feat: add vitest include configuration

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["src/**/*.spec.js"],
     coverage: {
+      include: ["src/**/*.js"],
+      exclude: ["src/**/*.spec.js"],
       provider: "v8",
       reporter: ["text", "html"],
       thresholds: {
