3d0aa21cfb0ae3b332a04b2049baf99f47506226
hjc <hjc_0827@outlook.com>
2026-03-01 14:07:55 +0800

    feat: Add the unit-test for b.js

---
+++ src/b.spec.js
@@ -0,0 +1,5 @@
+import { b } from "./b";
+
+it("should return the correct string", () => {
+  expect(b("test")).toBe("test b function");
+});
--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globals: true,
     include: ["src/**/*.spec.js"],
     coverage: {
       include: ["src/**/*.js"],
