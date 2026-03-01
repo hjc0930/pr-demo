f04e13176f3feefc7ea9b195920f88e768e84610
hjc <hjc_0827@outlook.com>
2026-03-01 13:11:36 +0800

    push test

--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,5 @@
 import { it, expect } from "vitest";
-import { add, subtract } from "./index";
+import { add, subtract, multiply, divide } from "./index";
 
 it("should add two numbers", () => {
   expect(add(1, 2)).toBe(3);
@@ -7,4 +7,12 @@ it("should add two numbers", () => {
 
 it("should subtract two numbers", () => {
   expect(subtract(5, 3)).toBe(2);
+});
+
+it("should multiply two numbers", () => {
+  expect(multiply(4, 6)).toBe(24);
+});
+
+it("should divide two numbers", () => {
+  expect(divide(10, 2)).toBe(5);
 });
